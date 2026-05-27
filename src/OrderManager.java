cde0e0eed44b5a8b2190b2d66c2bcc47901019fe
tiker <sebastian.dmochowski12@gmail.com>
2026-05-27 16:50:34 +0200

    klasa OrderManager skonczona

--- src/OrderManager.java
+++ src/OrderManager.java
@@ -1,21 +1,39 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class OrderManager {
     private List<Order> orders;
+    private int nextOrderId = 1;
+
+    public OrderManager(){
+        this.orders = new ArrayList<>();
+    }
 
     public Order createOrder(PricingRule rule){
-
+        Order newOrder = new Order(nextOrderId);
+        nextOrderId += 1;
+        newOrder.setPricingRule(rule);
+        this.orders.add(newOrder);
+        return newOrder;
     }
 
     public Order getOrder(int id){
-
+        for(Order order : this.orders){
+            if (order.getOrderID() == id){
+                return order;
+            }
+        }
+        return null;
     }
 
     public List<Order> getAllOrders() {
-
+        return this.orders;
     }
 
     public void cancelOrder(int id){
-
+        Order orderToCancel = getOrder(id);
+        if(orderToCancel != null){
+            orderToCancel.changeStatus(OrderStatus.CANCELLED);
+        }
     }
 }
