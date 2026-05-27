6bbfd91a4053a5901b87cbf70a2f0e68c778d55d
tiker <sebastian.dmochowski12@gmail.com>
2026-05-27 16:32:03 +0200

    Klasa Order skonczona

--- src/Order.java
+++ src/Order.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-enum orderStatus {
+enum OrderStatus {
     PENDING,
     IN_PROGRESS,
     COMPLETED,
@@ -11,14 +11,14 @@ enum orderStatus {
 public class Order {
 
     private int orderID;
-    private orderStatus status;
+    private OrderStatus status;
     private long createdAt;
     private PricingRule currentRule;
     private List<OrderItem> items = new ArrayList<>();
 
     public Order(int orderID){
         this.orderID = orderID;
-        this.status = orderStatus.PENDING;
+        this.status = OrderStatus.PENDING;
         this.createdAt = System.currentTimeMillis();
     }
 
@@ -33,16 +33,16 @@ public class Order {
         return currentRule.calculatePrice(this);
     }
 
-    public OrderItem addItem{
-        items.add()
+    public void addItem(OrderItem item){
+        this.items.add(item);
     }
 
-    public OrderItem removeItem{
-        items.remove()
+    public void removeItem(OrderItem item){
+        this.items.remove(item);
     }
 
-    public orderStatus changeStatus{
-
+    public void changeStatus(OrderStatus newStatus){
+        this.status = newStatus;
     }
 
     public List<OrderItem> getItems(){
@@ -51,6 +51,10 @@ public class Order {
 
     public int getOrderID(){
         return this.orderID;
+    }
+
+    public OrderStatus getOrderStatus(){
+        return this.status;
     }
 }
 
